5e406be8d9e66b9f581403cbfeebf7de71aec359
neonion12 <nion5404@gmail.com>
2023-11-09 00:18:28 +0600

    first commit

---
+++ .cph/Noldbach_problem.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define     Copy_maris_nah      int main()
+#define     OkkByee            return 0
+using namespace std;
+Copy_maris_nah
+{
+ int n,k;
+ cin>>n>>k;
+ int a[200];
+ for(int i=2;i<=1000;i++){
+    for(int j=2;j<=i;j++){
+        
+    }
+ }
+
+ OkkByee;
+}
