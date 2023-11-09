a91d353cef9a0df25f5d858da96f4f1f8282de8a
neonion12 <nion5404@gmail.com>
2023-11-09 22:36:45 +0600

    Noldbatch Commit

--- .cph/Noldbach_problem.cpp
+++ .cph/Noldbach_problem.cpp
@@ -1,17 +1,30 @@
-#include<bits/stdc++.h>
-#define     Copy_maris_nah      int main()
-#define     OkkByee            return 0
+#include <bits/stdc++.h>
+#define Ne_is_ON int main()
+#define OkkByee return 0
+using ll = long long;
 using namespace std;
-Copy_maris_nah
+Ne_is_ON
 {
- int n,k;
- cin>>n>>k;
- int a[200];
- for(int i=2;i<=1000;i++){
-    for(int j=2;j<=i;j++){
-        
-    }
- }
+   int n, k, l = 0;
+   cin >> n >> k;
+   int a[n / 2];
+   for (int i = 2; i <= n; i++)
+   {
+      int c = 1;
+      for (int j = 2; j * j <= i; j++)
+      {
+         if (i % j == 0)
+         {
+            c++;
+         }
+      }
+      if (c == 2 && l < k)
+      {
+         a[l] = i;
+         cout << a[l] << " ";
+         l++;
+      }
+   }
 
- OkkByee;
+   OkkByee;
 }
